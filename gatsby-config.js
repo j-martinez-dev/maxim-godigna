fda072f8de09218acf665be1265b9d4f5e8e52e2
$USER_NAME <jaime.martinez@grtgaz.com>
2020-05-29 21:27:34 +0200

    add google tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,17 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-168020458-1`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [`Montserrat`, `Cormorant+Garamond\:ital@1`],
